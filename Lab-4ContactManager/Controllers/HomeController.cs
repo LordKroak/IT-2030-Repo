26370bc36f3c3a97695558520e9ff923f173a0d7
Lord Kroak <jurassicgaming789@gmail.com>
2020-09-20 10:46:08 -0400

    Committing

--- Lab-4ContactManager/Controllers/HomeController.cs
+++ Lab-4ContactManager/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ContactManager.Models;
+using Lab_4ContactManager.Models;
 using System.Runtime.InteropServices.ComTypes;
 using ContactList.Models;
 
